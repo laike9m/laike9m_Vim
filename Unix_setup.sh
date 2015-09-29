261dce1625c3280b0b9a37c2e857bfefae6a8b30
laike9m <laike9m@gmail.com>
2015-09-29 12:05:15 -0400

    fix path

--- Unix_setup.sh
+++ Unix_setup.sh
@@ -1,4 +1,4 @@
 mkdir ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/laike9m_Vim/.vimrc ~/.vimrc
 vim +BundleInstall +qall
